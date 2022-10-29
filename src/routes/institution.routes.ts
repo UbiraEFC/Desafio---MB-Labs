c22f63f9326f7154fee79e8d8427c6b5b8a5fb15
UbiraEFC <ubirata.efc@gmail.com>
2022-10-29 07:56:05 -0300

    EnsureAuthenticated User, Institution and Both

--- src/routes/institution.routes.ts
+++ src/routes/institution.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { AuthenticateInstitutionController } from "../modules/institutions/services/authenticateInstitution/AuthenticateInstitutionController";
 import { CreateInstitutionController } from "../modules/institutions/services/createInstitution/CreateInstitutionController";
 import { CreateEventController } from "../modules/events/services/createEvent/CreateEventController";
-import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import { ensureAuthenticatedInstitution } from "../middlewares/ensureAuthenticatedInstitution";
 
 const institutionRoutes = Router();
 
@@ -12,6 +12,6 @@ const createEventController = new CreateEventController();
 
 institutionRoutes.post('/signup', createInstitutionController.signUp);
 institutionRoutes.post('/signin', authenticateInstitutionController.signIn);
-institutionRoutes.post('/event/create', ensureAuthenticated, createEventController.create);
+institutionRoutes.post('/event/create', ensureAuthenticatedInstitution, createEventController.create);
 
 export { institutionRoutes }
